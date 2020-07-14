8914fb1e00c07aeb9d31abfa36c70afe7fdec004
Alison Yoon <alison@Mickeys-MacBook-Pro.local>
2020-07-15 00:44:08 +0100

    find stunt double app created and added on git repo

---
+++ src/Workers.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export default [
+    {
+        stuntId: 1,
+        img: "./img/blackwidow.png",
+        name: "Black Widow"
+    },
+    {
+        stuntId: 2,
+        img: "./img/deadpool.png",
+        name: "Dead Pool"
+    },
+    {
+        stuntId: 3,
+        img: "./img/drstrange.png",
+        name: "Dr. Strange"
+    },
+    {
+        stuntId: 4,
+        img: "./img/ironman.png",
+        name: "Iron Man"
+    },
+    {
+        stuntId: 5,
+        img: "./img/kyloren.png",
+        name: "Kylo Ren"
+    },
+    {
+        stuntId: 6,
+        img: "./img/rey.png",
+        name: "Rey"
+    }
+];
+
